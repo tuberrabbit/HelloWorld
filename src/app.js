6c77a97420dca4cd725d437d05549a5d55b5d2f7
tuber <btu@nocode.com>
2021-01-08 20:28:06 +0800

    perf: auto refresh cookie & token

--- src/app.js
+++ src/app.js
@@ -1,50 +1,54 @@
-const {Builder, By, Key, until} = require('selenium-webdriver');
-// 注意 ID 要倒序，留意更新 cookie 和 token
-const token = 96709732;
-const cookie = 'cookie';
-const idList = [10,9,8];
+const { Builder, By, Key, until } = require('selenium-webdriver');
+const qs = require('qs');
+// 注意 ID 要倒序
+const idList = [1];
 
-function getCookies() {
-  const list = cookie.split('; ');
-  const result = list.map(i=>{
-    const [name,value] = i.replace(/=/, '; ').split('; ');
-    return {name,value};
-  });
-  return result;
-}
 (async function example() {
   let driver = await new Builder().forBrowser('chrome').build();
   try {
-    await driver.get(`https://mp.weixin.qq.com/wxamp/basicprofile/relation?token=${token}&lang=zh_CN`);
-    const cookies = getCookies();
+    await driver.get(`https://mp.weixin.qq.com/`);
+    const { cookies, token } = await new Promise((resolve, reject) => {
+      let count = 0;
+      const timer = setInterval(async () => {
+        const url = await driver.getCurrentUrl();
+        if (/wxamp\/basicprofile\/relation/.test(url)) {
+          const cookies = await driver.manage().getCookies();
+          const { token } = qs.parse(url.split('?')[1]);
+          if (cookies && cookies.length > 5) {
+            clearInterval(timer);
+            resolve({ cookies, token });
+          }
+        }
+        count++;
+        if (count > 10) {
+          clearInterval(timer);
+          reject('登录超时');
+        }
+      }, 3000);
+    });
     for (const item of cookies) {
-      await driver.manage().addCookie({ name:item.name, value: item.value});
+      await driver.manage().addCookie({ name: item.name, value: item.value });
     }
     await driver.get(`https://mp.weixin.qq.com/wxamp/basicprofile/relation?token=${token}&lang=zh_CN`);
     for (let i = 0; i < idList.length; i++) {
       const page = Math.ceil(idList[i] / 10);
-      const key = (idList[i]-1) % 10+1;
+      const key = (idList[i] - 1) % 10 + 1;
       const input = await driver.wait(until.elementLocated(By.css('.weui-desktop-pagination__input')), 10000);
       await input.sendKeys(page, Key.ENTER);
-      await new Promise(resolve => {
-        setTimeout(resolve, 3000);
-      });
+      await driver.sleep(3000);
       const boxes = await driver.wait(until.elementsLocated(By.css('.mod_default_box')), 10000);
       const rows = await boxes[1].findElements(By.css('tbody tr'));
-      const cell = await rows[key-1].findElement(By.css('.table_cell .js_mp_unbind'));
+      const cell = await rows[key - 1].findElement(By.css('.table_cell .js_mp_unbind'));
       await cell.click();
-      await driver.wait(async()=>{
-        try{
-          const target = await driver.findElement(By.css('.weui-desktop-pagination__input'));
-          return await target.getAttribute("value")==='';
-        } catch (e) {
-          return false;
-        }
+      await driver.wait(async () => {
+        const target = await driver.findElement(By.css('.weui-desktop-pagination__input'));
+        return await target.getAttribute("value") === '';
       }, 30000);
-      console.log(idList[i],'ok');
+      console.log(idList[i], 'ok');
     }
-  }
-  finally{
+  } catch (e) {
+    console.error(e);
+  } finally {
     await driver.quit();
   }
 })();
